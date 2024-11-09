2efd0a0111a16efbe023817b46e364cd5b8cb5e0
BigWiinner <150750398+BigWiinner@users.noreply.github.com>
2024-11-08 20:00:39 -0800

    Made it so that there is always an enemy right behind you (#34)
    So long as the player is looking to the right that is lmao
    
    Co-authored-by: Jonah Ryan <jonahrobot@gmail.com>

--- Assets/CONTENT/Programming/EnemyRunner.cs
+++ Assets/CONTENT/Programming/EnemyRunner.cs
@@ -13,6 +13,11 @@ public class EnemyRunner : MonoBehaviour
     // Internal Logic
     Action randomAction;
 
+    
+    // I added this for EnemyUpdate_Mine
+    float timer = 0.0f;
+    // - NateWii
+
     // START CICI CODE
     Vector3 targetPos;
     bool start = true;
@@ -210,6 +215,17 @@ public class EnemyRunner : MonoBehaviour
         }
     }
 
+    public void EnemyUpdate_Mine() 
+    {
+        enemyRB.gravityScale = 0;
+        enemyRB.MovePosition(Vector3.MoveTowards(transform.position, player.transform.position, 12f * Time.deltaTime));
+        if (timer >= 2.0f) {
+            timer = 0.0f;
+            Instantiate(enemyRB, new Vector3(player.transform.position.x - 3.0f, player.transform.position.y + 1.0f, 0.0f), Quaternion.identity);
+        }
+        timer += Time.deltaTime;
+    }
+
     /// ADD YOUR SCRIPT ABOVE! ------------------------
 
 
@@ -224,6 +240,9 @@ public class EnemyRunner : MonoBehaviour
         /// ADD YOUR SCRIPT BELOW TO ADD!
         list.Add(EnemyUpdate_Example_A);
         list.Add(EnemyUpdate_Example_B);
+
+        list.Add(EnemyUpdate_Mine);
+
 
         list.Add(Tate_Monster);
 
