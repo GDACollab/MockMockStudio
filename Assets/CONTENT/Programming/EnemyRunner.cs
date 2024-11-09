31c04186378f6101a716c6cb11fc263f37507893
empaster <empaster@ucsc.edu>
2024-11-08 20:14:03 -0800

    Added extra enemy script

--- Assets/CONTENT/Programming/EnemyRunner.cs
+++ Assets/CONTENT/Programming/EnemyRunner.cs
@@ -13,6 +13,8 @@ public class EnemyRunner : MonoBehaviour
     // Internal Logic
     Action randomAction;
 
+    // Added Variables
+    float currentSpeed;
     
     /*
      * Please do not modify anything but the sections between the /// comments!
@@ -30,6 +32,30 @@ public class EnemyRunner : MonoBehaviour
         enemyRB.MovePosition(Vector3.MoveTowards(transform.position, player.transform.position, 2f * Time.deltaTime));
     }
 
+    public void EnemyUpdate_Erik()
+    {
+        //enemyRB.gravityScale = 0;
+        //float speedRandomizer = UnityEngine.Random.Range(0.9F, 1.1F);
+        //currentSpeed *= speedRandomizer;
+        
+        enemyRB.MovePosition(Vector3.MoveTowards(transform.position, player.transform.position, currentSpeed * Time.deltaTime));
+
+        int coinFlip = UnityEngine.Random.Range(0, 2);
+        Vector3 scaler = new Vector3(0, 0, 0);
+        int maxSize = 3;
+
+        if (coinFlip == 0 && transform.localScale.x < maxSize)
+        {
+            scaler = new Vector3(0.5F, 0.5F, 0);
+        }
+        else if (coinFlip == 1 && transform.localScale.x >= maxSize)
+        {
+            scaler = new Vector3(-0.5F, -0.5F, 0);
+        }
+
+        transform.localScale += scaler;
+    }
+
     /// ADD YOUR SCRIPT ABOVE! ------------------------
 
 
@@ -42,8 +68,13 @@ public class EnemyRunner : MonoBehaviour
         var list = new List<Action>();
 
         /// ADD YOUR SCRIPT BELOW TO ADD!
-        list.Add(EnemyUpdate_Example_A);
-        list.Add(EnemyUpdate_Example_B);
+        //list.Add(EnemyUpdate_Example_A);
+        //list.Add(EnemyUpdate_Example_B);
+
+        enemyRB.gravityScale = 0;
+        currentSpeed = 5;
+        list.Add(EnemyUpdate_Erik);
+
         /// ADD YOUR SCRIPT ABOVE TO ADD!
 
         var randomIndex = UnityEngine.Random.Range(0, list.Count);
