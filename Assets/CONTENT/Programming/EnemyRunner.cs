bebbb783b1c2862e6879465efdf71e4c2e0c4ba7
BigWiinner <150750398+BigWiinner@users.noreply.github.com>
2024-11-08 19:46:14 -0800

    Made it so that there is always an enemy right behind you
    So long as the player is looking to the right that is lmao

--- Assets/CONTENT/Programming/EnemyRunner.cs
+++ Assets/CONTENT/Programming/EnemyRunner.cs
@@ -12,7 +12,10 @@ public class EnemyRunner : MonoBehaviour
 
     // Internal Logic
     Action randomAction;
-
+    
+    // I added this for EnemyUpdate_Mine
+    float timer = 0.0f;
+    // - NateWii
     
     /*
      * Please do not modify anything but the sections between the /// comments!
@@ -30,6 +33,17 @@ public class EnemyRunner : MonoBehaviour
         enemyRB.MovePosition(Vector3.MoveTowards(transform.position, player.transform.position, 2f * Time.deltaTime));
     }
 
+    public void EnemyUpdate_Mine() 
+    {
+        enemyRB.gravityScale = 0;
+        enemyRB.MovePosition(Vector3.MoveTowards(transform.position, player.transform.position, 12f * Time.deltaTime));
+        if (timer >= 2.0f) {
+            timer = 0.0f;
+            Instantiate(enemyRB, new Vector3(player.transform.position.x - 3.0f, player.transform.position.y + 1.0f, 0.0f), Quaternion.identity);
+        }
+        timer += Time.deltaTime;
+    }
+
     /// ADD YOUR SCRIPT ABOVE! ------------------------
 
 
@@ -44,6 +58,7 @@ public class EnemyRunner : MonoBehaviour
         /// ADD YOUR SCRIPT BELOW TO ADD!
         list.Add(EnemyUpdate_Example_A);
         list.Add(EnemyUpdate_Example_B);
+        list.Add(EnemyUpdate_Mine);
         /// ADD YOUR SCRIPT ABOVE TO ADD!
 
         var randomIndex = UnityEngine.Random.Range(0, list.Count);
