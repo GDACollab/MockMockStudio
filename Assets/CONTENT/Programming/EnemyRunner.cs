eed30a6342d38d31fd5298bcf127d57678b26ca3
AshtonTankGallistel <123789343+AshtonTankGallistel@users.noreply.github.com>
2024-11-08 20:02:08 -0800

    Added new enemy code (#32)
    The new code has the enemy move on the floor, jump, then dash at the player. It doesn't really work, apologies.
    
    Co-authored-by: Jonah Ryan <jonahrobot@gmail.com>

--- Assets/CONTENT/Programming/EnemyRunner.cs
+++ Assets/CONTENT/Programming/EnemyRunner.cs
@@ -13,7 +13,12 @@ public class EnemyRunner : MonoBehaviour
     // Internal Logic
     Action randomAction;
 
-    
+
+    //ASHTON'S VARIABLES START
+    float AshtonsTimer = 0;
+    bool AshtonsBool = false;
+    //ASHTON'S VARIABLES END
+
     // I added this for EnemyUpdate_Mine
     float timer = 0.0f;
     // - NateWii
@@ -53,6 +58,57 @@ public class EnemyRunner : MonoBehaviour
         enemyRB.gravityScale = 0;
         enemyRB.MovePosition(Vector3.MoveTowards(transform.position, player.transform.position, 2f * Time.deltaTime));
     }
+
+    //This function has the enemy walk on the ground. After a bit, it will jump up high, then launch at the player
+    public void EnemyUpdate_By_Ashton()
+    {
+        LayerMask groundMask = LayerMask.GetMask("Ground");
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.55f, layerMask: groundMask);
+        if(hit.collider == null)
+        {
+            enemyRB.gravityScale = 3;
+            //Debug.Log("AA");
+        }
+        else
+        {
+            //Debug.Log("BB");
+            enemyRB.gravityScale = 0;
+            if(enemyRB.velocity.y < 0)
+            {
+                enemyRB.velocity = new Vector2(enemyRB.velocity.x, 0);
+            }
+        }
+        RaycastHit2D hit2 = Physics2D.Raycast(transform.position, Vector2.up, 0.55f, layerMask: groundMask);
+        if(hit2.collider != null)
+        {
+            enemyRB.gravityScale = -3;
+        }
+        AshtonsTimer += Time.deltaTime;
+        //enemyRB.MovePosition(Vector3.MoveTowards(transform.position, player.transform.position, 1f * Time.deltaTime));
+        if (player.transform.position.x > transform.position.x)
+        {
+            enemyRB.velocity = new Vector2(1, enemyRB.velocity.y);
+        }
+        else
+            enemyRB.velocity = new Vector2(-1, enemyRB.velocity.y);
+        if (AshtonsTimer > 3)
+        {
+            if (!AshtonsBool)
+            {
+                //Debug.Log("JUMP");
+                enemyRB.AddForce(Vector2.up * 1750);
+                AshtonsBool = true;
+            }
+            else if(AshtonsTimer >= 3.5f)
+            {
+                enemyRB.AddForce(3250 * (new Vector2(player.transform.position.x - transform.position.x, (player.transform.position.y - transform.position.y) / 4).normalized));
+                AshtonsTimer = 0;
+                AshtonsBool = false;
+            }
+        }
+    }
+
+
     public void EnemyUpdateUhhh()
     {
         enemyRB.gravityScale = -1;
@@ -226,6 +282,7 @@ public class EnemyRunner : MonoBehaviour
         timer += Time.deltaTime;
     }
 
+
     /// ADD YOUR SCRIPT ABOVE! ------------------------
 
 
@@ -240,6 +297,9 @@ public class EnemyRunner : MonoBehaviour
         /// ADD YOUR SCRIPT BELOW TO ADD!
         list.Add(EnemyUpdate_Example_A);
         list.Add(EnemyUpdate_Example_B);
+
+        list.Add(EnemyUpdate_By_Ashton);
+
 
         list.Add(EnemyUpdate_Mine);
 
