8b7942313687e94ca5ef6f3be5e52c2a7bd2037d
Tate Maguire <63537750+tatemaguire@users.noreply.github.com>
2024-11-08 19:45:57 -0800

    made my enemy script

--- Assets/CONTENT/Programming/EnemyRunner.cs
+++ Assets/CONTENT/Programming/EnemyRunner.cs
@@ -13,7 +13,7 @@ public class EnemyRunner : MonoBehaviour
     // Internal Logic
     Action randomAction;
 
-    
+
     /*
      * Please do not modify anything but the sections between the /// comments!
      */
@@ -30,6 +30,68 @@ public class EnemyRunner : MonoBehaviour
         enemyRB.MovePosition(Vector3.MoveTowards(transform.position, player.transform.position, 2f * Time.deltaTime));
     }
 
+
+    // TateM-Prog-EnemyScript
+    private float tmAngle = 0; //angle in radians
+    private float tmRadius = 2;
+    private float tmTimer = 0;
+    private float tmX = 0;
+    private float tmY = 0;
+    private bool tmAttacking = false;
+    public void Tate_Monster()
+    {
+        enemyRB.gravityScale = 0;
+
+        // update angle
+        tmAngle += 1 * Time.deltaTime;
+        if (tmAngle >= Mathf.PI)
+        {
+            tmAngle -= 2 * Mathf.PI;
+        }
+
+        if (!tmAttacking)
+        {
+            // update timer
+            tmTimer += Time.deltaTime;
+            if (tmTimer > 3)
+            {
+                print(tmTimer);
+                tmAttacking = true;
+                tmTimer = 0;
+            }
+
+            // calculate positions
+            tmX = player.transform.position.x + tmRadius * Mathf.Cos(tmAngle);
+            tmY = player.transform.position.y + tmRadius * Mathf.Sin(tmAngle);
+
+            // update position
+            Vector2 position = new Vector2(tmX, tmY);
+            enemyRB.MovePosition(position);
+        }
+        else
+        {
+            // change radius
+            if (tmRadius > -2)
+            {
+                tmRadius -= 0.1f * Time.deltaTime;
+            }
+            else
+            {
+                tmRadius = 2;
+                tmAngle += Mathf.PI;
+                tmAttacking = false;
+            }
+
+            // calculate positions
+            tmX = player.transform.position.x + tmRadius * Mathf.Cos(tmAngle);
+            tmY = player.transform.position.y + tmRadius * Mathf.Sin(tmAngle);
+
+            // update position
+            Vector2 position = new Vector2(tmX, tmY);
+            enemyRB.MovePosition(position);
+        }
+    }
+
     /// ADD YOUR SCRIPT ABOVE! ------------------------
 
 
@@ -44,6 +106,7 @@ public class EnemyRunner : MonoBehaviour
         /// ADD YOUR SCRIPT BELOW TO ADD!
         list.Add(EnemyUpdate_Example_A);
         list.Add(EnemyUpdate_Example_B);
+        list.Add(Tate_Monster);
         /// ADD YOUR SCRIPT ABOVE TO ADD!
 
         var randomIndex = UnityEngine.Random.Range(0, list.Count);
